5285a11700676efc459bbd785809947dae5cd31b
Sungtae Kim <pchero21@gmail.com>
2017-05-10 01:27:45 +0200

    Fixed memory leak
    - #57
    - Done

--- backend/src/event_handler.h
+++ backend/src/event_handler.h
@@ -9,7 +9,11 @@
 #define BACKEND_SRC_EVENT_HANDLER_H_
 
 #include <stdbool.h>
+#include <event.h>
 
 bool init_event_handler(void);
+void term_event_handler(void);
+
+void add_event_handler(struct event* ev);
 
 #endif /* BACKEND_SRC_EVENT_HANDLER_H_ */
