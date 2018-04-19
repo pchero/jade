21d15016432533ed5e8ea8360dce64564f6d72af
Sungtae Kim <pchero21@gmail.com>
2018-04-19 18:33:29 +0200

    Added admin handler module template
    - #198

---
+++ src/includes/admin_handler.h
@@ -0,0 +1,15 @@
+/*
+ * admin_handler.h
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: pchero
+ */
+
+#ifndef SRC_INCLUDES_ADMIN_HANDLER_H_
+#define SRC_INCLUDES_ADMIN_HANDLER_H_
+
+
+
+
+
+#endif /* SRC_INCLUDES_ADMIN_HANDLER_H_ */
---
+++ src/modules/admin_handler.c
@@ -0,0 +1,8 @@
+/*
+ * admin_handler.c
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: pchero
+ */
+
+#include "admin_handler.h"
