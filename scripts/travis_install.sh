45c5da8bb44d0bc86cc8d24556408ab4b66ac5c5
Sungtae Kim <pchero21@gmail.com>
2018-05-26 17:29:25 +0200

    Travis stuffs
    - WIP

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -7,21 +7,21 @@ then
   sudo apt-get install -y -qq build-essential openssl libxml2-dev libncurses5-dev uuid-dev sqlite3 libsqlite3-dev pkg-config curl libjansson-dev 
   sudo apt-get install -y -qq libssl-dev git cmake libevent-dev libjansson-dev libbsd-dev libzmq5-dev libonig-dev
   
-  # libevhtp
-  sudo mkdir -p /opt/src/libevhtp
-  sudo curl -s https://codeload.github.com/criticalstack/libevhtp/tar.gz/1.2.16 | tar xz -C /opt/src/libevhtp --strip-components=1
-  cd /opt/src/libevhtp/build
-  sudo cmake ..
-  sudo make
-  sudo make install
+#  # libevhtp
+#  sudo mkdir -p /opt/src/libevhtp
+#  sudo curl -s https://codeload.github.com/criticalstack/libevhtp/tar.gz/1.2.16 | tar xz -C /tmp/libevhtp --strip-components=1
+#  cd /opt/src/libevhtp/build
+#  sudo cmake ..
+#  sudo make
+#  sudo make install
 
-  # libwebsockets  
-  sudo mkdir -p /opt/src/libwebsockets
-  sudo curl -s https://codeload.github.com/warmcat/libwebsockets/tar.gz/v2.4.2 | tar xz -C /opt/src/libwebsockets --strip-components=1
-  cd /opt/src/libwebsockets/build
-  sudo cmake -DLWS_WITH_LIBEVENT=1 ../
-  sudo make
-  sudo make install
+#  # libwebsockets  
+#  sudo mkdir -p /opt/src/libwebsockets
+#  sudo curl -s https://codeload.github.com/warmcat/libwebsockets/tar.gz/v2.4.2 | tar xz -C /opt/src/libwebsockets --strip-components=1
+#  cd /opt/src/libwebsockets/build
+#  sudo cmake -DLWS_WITH_LIBEVENT=1 ../
+#  sudo make
+#  sudo make install
   
 fi
 
