9eeeac1ca5217ca430f8c41e385a43be28b7d855
Sungtae Kim <pchero21@gmail.com>
2018-05-26 17:50:15 +0200

    Update travis_install.sh

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -7,6 +7,7 @@ then
   sudo apt-get install -y -qq build-essential openssl libxml2-dev libncurses5-dev uuid-dev sqlite3 libsqlite3-dev pkg-config curl libjansson-dev 
   sudo apt-get install -y -qq curl libssl-dev git cmake libevent-dev libjansson-dev libbsd-dev libzmq-dev libonig-dev
   
+  
   # libevhtp
   sudo mkdir -p /opt/src/libevhtp
   sudo curl -s https://codeload.github.com/criticalstack/libevhtp/tar.gz/1.2.16 | sudo tar xz -C /opt/src/libevhtp --strip-components=1
@@ -14,6 +15,7 @@ then
   sudo cmake ..
   sudo make
   sudo make install
+
 
   # libwebsockets  
   sudo mkdir -p /opt/src/libwebsockets
